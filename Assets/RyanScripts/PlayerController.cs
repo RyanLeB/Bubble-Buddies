52fdd16747dbc4961e585619f17acab21ee49295
Ryan LeBlanc <ryanleb2003@gmail.com>
2025-01-25 14:06:23 -0400

    fixed collision issue

--- Assets/RyanScripts/PlayerController.cs
+++ Assets/RyanScripts/PlayerController.cs
@@ -3,27 +3,39 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 2f;
-    public float floatSpeed = 5f; 
-    public float gravity = 2f; 
+    public float floatSpeed = 5f;
+    public float gravity = 2f;
+    public Vector2 minBounds;
+    public Vector2 maxBounds;
     private Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rb.isKinematic = true; 
     }
 
     void Update()
     {
         
-        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        Vector2 velocity = new Vector2(moveSpeed, rb.velocity.y);
 
         
-        rb.velocity += Vector2.down * gravity * Time.deltaTime;
+        velocity += Vector2.down * gravity * Time.deltaTime;
 
         
         if (Input.GetKey(KeyCode.Space))
         {
-            rb.velocity = new Vector2(rb.velocity.x, floatSpeed);
+            velocity = new Vector2(velocity.x, floatSpeed);
         }
+
+        
+        rb.velocity = velocity;
+
+        
+        Vector2 position = rb.position;
+        
+        position.y = Mathf.Clamp(position.y, minBounds.y, maxBounds.y);
+        rb.position = position;
     }
 }
