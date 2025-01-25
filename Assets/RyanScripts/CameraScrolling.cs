e6ed38282519907d4c6571f0e64fd69262e39967
Ryan LeBlanc <ryanleb2003@gmail.com>
2025-01-25 12:55:16 -0400

    small gameplay changes

--- Assets/RyanScripts/CameraScrolling.cs
+++ Assets/RyanScripts/CameraScrolling.cs
@@ -4,16 +4,24 @@ public class CameraScrolling : MonoBehaviour
 {
     public Transform player;
     public float offset = 5f; 
+    [SerializeField] private LevelManager levelManager;
+
 
     void Update()
     {
-        if (player != null)
+        if(levelManager.scenename == "Game")
         {
-            Vector3 newPosition = player.position;
-            newPosition.x += offset; 
-            newPosition.y = transform.position.y; 
-            newPosition.z = transform.position.z; 
-            transform.position = newPosition;
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+            FollowPlayer();
         }
+    }
+
+    void FollowPlayer()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.x += offset; 
+        newPosition.y = transform.position.y; 
+        newPosition.z = transform.position.z; 
+        transform.position = newPosition;
     }
 }
